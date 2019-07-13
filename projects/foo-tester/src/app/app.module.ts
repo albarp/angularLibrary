e515b207bca105344087fd21137b83c97b8006d9
Alberto A <alberto.arpini@fmc-ag.com>
2019-07-13 21:51:22 +0200

    Used library in tester app

--- projects/foo-tester/src/app/app.module.ts
+++ projects/foo-tester/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooLibModule } from 'projects/foo-lib/src/public_api';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FooLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
